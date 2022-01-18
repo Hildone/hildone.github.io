1128500994210a311a4e33465e61ca58bf251856
Hilal Haidar <81667329+Hildone@users.noreply.github.com>
2022-01-18 21:30:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
